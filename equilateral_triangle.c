1b2f40de77d04ed7ff91978bcc4ffff5a81be700
vedam-parthiv-kumar <vedamparthivkumar06@gmail.com>
2026-02-16 23:30:15 +0530

    Create equilateral_triangle.c

---
+++ equilateral_triangle.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int i, space, j, rows = 5;
+    for(i = 1; i <= rows; i++) {
+        for(space = 1; space <= rows-i; space++)
+            printf(" ");
+        for(j = 1; j <= (2*i-1); j++)
+            printf("*");
+        printf("\n");
+    }
+    return 0;
+}
